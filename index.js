c4940d681141336f6aa16826c3b5ccaa3699a418
RomainRG31 <romain32.roger@gmail.com>
2024-05-28 07:13:24 +0200

    modif cow for eyes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const format = require("./lib/format");
 const data = require("./lib/data");
 
-module.exports = ({ json, cow = "squirrel" }) =>
+module.exports = ({ json, cow = "eyes" }) =>
   json ? JSON.stringify(data) : format({ data, cow });
